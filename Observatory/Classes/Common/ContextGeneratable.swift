6def7c5f53f78c127b2da844b7fec95a09309fe4
RavenDeng <dlfkid@icloud.com>
2023-10-05 17:15:30 +0800

    feat: Add ID Generate mech

--- Observatory/Classes/Common/ContextGeneratable.swift
+++ Observatory/Classes/Common/ContextGeneratable.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+
+protocol TelemetryID {
+    var raw: [UInt8] {get}
+}
+
+extension TelemetryID {
+    var bytes: Data {
+        return Data(raw)
+    }
+    
+    var string: String {
+        return String(bytes: raw, encoding: .utf8) ?? ""
+    }
+}
+
+struct TraceID: TelemetryID {
+    var raw: [UInt8]
+}
+
+struct SpanID: TelemetryID {
+    var raw: [UInt8]
+}
+
+protocol ContextGenerateable {
+}
+
+extension ContextGenerateable {
+    
+    func generateTraceID() -> TraceID {
+        return TraceID(raw: generateSecureRandomBytes(length: 16))
+    }
+    
+    func generateSpanID() -> SpanID {
+        return SpanID(raw: generateSecureRandomBytes(length: 8))
+    }
+    
+    private func generateSecureRandomBytes(length: Int) -> [UInt8] {
+        var randomBytes = [UInt8](repeating: 0, count: length)
+        
+        let status = SecRandomCopyBytes(kSecRandomDefault, length, &randomBytes)
+        
+        if status == errSecSuccess {
+            return randomBytes
+        } else {
+            var randomBytes = [UInt8](repeating: 0, count: length)
+                
+            for i in 0..<length {
+                let randomValue = arc4random_uniform(256)
+                randomBytes[i] = UInt8(randomValue)
+            }
+            
+            return randomBytes
+        }
+    }
+}
+
---
+++ Observatory/Classes/Logging/Model/LogRecordData.swift
@@ -0,0 +1,8 @@
+//
+//  LogRecordData.swift
+//  Observatory
+//
+//  Created by LeonDeng on 2023/10/5.
+//
+
+import Foundation
