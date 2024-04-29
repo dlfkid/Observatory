420cd3576b0312985c47e198c27cb9f5d6ff32fa
Ravendeng <dlfkid@icloud.com>
2024-04-29 10:37:03 +0800

    Fix: [Export] Add codable adaption for Resource and scope
    1. Resource construct refactor
    2. Scope codable

--- Sources/Common/Data/Resource.swift
+++ Sources/Common/Data/Resource.swift
@@ -8,81 +8,84 @@
 import Foundation
 
 public struct Resource {
+    private let droppedAttribute: Int
     
-    private let serviceNameKey = "service.name"
+    private let attributes: [ObservatoryKeyValue]
     
-    private let nameSpaceKey = "service.namespace"
-    
-    private let instanceIdKey = "service.instance.id"
-    
-    private let versionKey = "service.version"
-    
-    fileprivate var serviceName: String?
-    fileprivate var nameSpace: String?
-    fileprivate var instanceId: String?
-    fileprivate var version: String?
-    fileprivate var otherAttributes: [String: ObservatoryValue]?
-    
-    var metaData: [String: ObservatoryValue] {
-        var result = [String: ObservatoryValue]()
-        if let otherAttributes = otherAttributes {
-            otherAttributes.forEach { (key: String, value: ObservatoryValue) in
-                result[key] = value
-            }
-        }
-        result[serviceNameKey] = .string(serviceName ?? "")
-        result[nameSpaceKey] = .string(nameSpace ?? "")
-        result[instanceIdKey] = .string(instanceId ?? "")
-        result[versionKey] = .string(version ?? "")
-        return result
-    }
-    
-    fileprivate init(serviceName: String? = nil, nameSpace: String? = nil, instanceId: String? = nil, version: String? = nil, otherAttributes: [String : ObservatoryValue]? = nil) {
-        self.serviceName = serviceName
-        self.nameSpace = nameSpace
-        self.instanceId = instanceId
-        self.version = version
-        self.otherAttributes = otherAttributes
+    fileprivate init(droppedAttribute: Int, attributes: [ObservatoryKeyValue]) {
+        self.droppedAttribute = droppedAttribute
+        self.attributes = attributes
     }
 }
 
 public class ResourceBuilder {
-    var limit = LimitConfig()
-    var resourceSample = Resource()
+    private let serviceNameKey = "service.name"
+    private let nameSpaceKey = "service.namespace"
+    private let instanceIdKey = "service.instance.id"
+    private let versionKey = "service.version"
+    
+    private var limit = LimitConfig()
+    private var attributesDict = [String: ObservatoryValue]()
+    private var droppedAttribute: Int = 0
     
     public init() {}
     
     public func serviceName(_ serviceName: String) -> ResourceBuilder {
-        resourceSample.serviceName = serviceName
+        attributesDict[serviceNameKey] = .string(serviceName)
         return self
     }
     public func nameSpace(_ nameSpace: String) -> ResourceBuilder {
-        resourceSample.nameSpace = nameSpace
+        attributesDict[nameSpaceKey] = .string(nameSpace)
         return self
     }
     public func instanceId(_ instanceId: String) -> ResourceBuilder {
-        resourceSample.instanceId = instanceId
+        attributesDict[instanceIdKey] = .string(instanceId)
         return self
     }
     public func version(_ version: String) -> ResourceBuilder {
-        resourceSample.version = version
+        attributesDict[versionKey] = .string(version)
         return self
     }
     public func otherAttributes(_ otherAttributes: [String: ObservatoryValue]) -> ResourceBuilder {
         var attributeCount = 0
         for (key, value) in otherAttributes {
-            if attributeCount >= limit.maximumNumberOfAttributes ?? 0 {
-                // dropped attribute
+            if let value = attributesDict[key] {
+                // if key is already exist, replace the key
+                attributesDict[key] = value
                 continue
             }
-            resourceSample.otherAttributes?[key] = value
+            if attributeCount >= limit.maximumNumberOfAttributes ?? 0 {
+                // if the dict is reach its limit, dropp the attribute
+                droppedAttribute += 1
+                continue
+            }
+            // add the attribute normally
+            attributesDict[key] = value
             attributeCount += 1
         }
         return self
     }
     
     public func build() -> Resource {
-        return resourceSample
+        var attributes = [ObservatoryKeyValue]()
+        for (key, value) in attributesDict {
+            let keyValue = ObservatoryKeyValue(key: key, value: value)
+            attributes.append(keyValue)
+        }
+        return Resource(droppedAttribute: droppedAttribute, attributes: attributes)
+    }
+}
+
+extension Resource: Encodable {
+    enum CodingKeys: String, CodingKey {
+        case dropped_attributes_count = "dropped_attributes_count"
+        case attributes = "attributes"
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(attributes, forKey: .attributes)
+        try container.encode(0, forKey: .dropped_attributes_count)
     }
 }
 
--- Sources/Common/GlobalDefines/Scopable.swift
+++ Sources/Common/GlobalDefines/Scopable.swift
@@ -9,9 +9,31 @@ import Foundation
 
 public struct InstrumentationScope: Hashable, Equatable {
   // An empty instrumentation scope name means the name is unknown.
-    let name: String
-    let version: String
+    let name: String?
+    let version: String?
     let schemaURL: String?
+}
+
+extension InstrumentationScope: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        version = try container.decode(String.self, forKey: .version)
+        schemaURL = try container.decodeIfPresent(String.self, forKey: .schemaURL)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(version, forKey: .version)
+        try container.encode(schemaURL, forKey: .schemaURL)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case version
+        case schemaURL
+    }
 }
 
 public protocol Scopable {
