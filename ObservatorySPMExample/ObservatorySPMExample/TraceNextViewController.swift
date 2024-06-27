4ab66dbdc2b332702eb786f4eef63a5aa6b07393
Ravendeng <dlfkid@icloud.com>
2024-06-27 19:53:24 +0800

    fix: [Demo] Tracing demo add button

--- ObservatorySPMExample/ObservatorySPMExample/TraceNextViewController.swift
+++ ObservatorySPMExample/ObservatorySPMExample/TraceNextViewController.swift
@@ -21,39 +21,55 @@ class TraceNextViewController: UIViewController {
     var superSpan: ReadableSpan?
     
     private var span: ReadableSpan?
+    
+    private lazy var actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Trace action", for: .normal)
+        button.addTarget(self, action: #selector(traceActionButtonAction), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(actionButton)
+        actionButton.frame = CGRectMake(0, 0, 120, 55)
+        actionButton.center = view.center
         view.backgroundColor = .white
         title = "Tracing Next Page"
         let sampleAttri = [ObservatoryKeyValue(key: "controller_name", value: .string("TraceNextViewController"))]
         span = self.obs.spanStart(name: "demo_life_cycle", kind: .client, parentContext: superSpan?.context, attributes: sampleAttri, traceStateStr: nil)
-        span?.addEvent(name: "life_cycle_event", attributes: [ObservatoryKeyValue(key: "name", value: .string("viewDidLoad"))])
+        span?.addEvent(name: "viewDidLoad", attributes: [ObservatoryKeyValue(key: "name", value: .string("viewDidLoad"))])
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        span?.addEvent(name: "life_cycle_event", attributes: [ObservatoryKeyValue(key: "name", value: .string("viewWillAppear"))])
+        span?.addEvent(name: "viewWillAppear", attributes: [ObservatoryKeyValue(key: "name", value: .string("viewWillAppear"))])
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        span?.addEvent(name: "life_cycle_event", attributes: [ObservatoryKeyValue(key: "name", value: .string("viewDidAppear"))])
+        span?.addEvent(name: "viewDidAppear", attributes: [ObservatoryKeyValue(key: "name", value: .string("viewDidAppear"))])
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        span?.addEvent(name: "life_cycle_event", attributes: [ObservatoryKeyValue(key: "name", value: .string("viewWillDisappear"))])
+        span?.addEvent(name: "viewWillDisappear", attributes: [ObservatoryKeyValue(key: "name", value: .string("viewWillDisappear"))])
     }
 
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        span?.addEvent(name: "life_cycle_event", attributes: [ObservatoryKeyValue(key: "name", value: .string("viewDidDisappear"))])
+        span?.addEvent(name: "viewDidDisappear", attributes: [ObservatoryKeyValue(key: "name", value: .string("viewDidDisappear"))])
     }
     
     deinit {
-        span?.addEvent(name: "life_cycle_event", attributes: [ObservatoryKeyValue(key: "name", value: .string("deinit"))])
+        span?.addEvent(name: "deinit", attributes: [ObservatoryKeyValue(key: "name", value: .string("deinit"))])
         span?.end()
     }
 
+}
+
+extension TraceNextViewController {
+    @objc private func traceActionButtonAction() {
+        span?.addEvent(name: "TraceActionButtonTapped", attributes: nil)
+    }
 }
